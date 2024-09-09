e554b017712c9b634a5b1215f4fc5cea5a0410c8
Nathalia Ruffo <146651658+NathaliaRuffo@users.noreply.github.com>
2024-09-09 15:48:07 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+function pesquisar() {
+    // Seleciona a seção onde os resultados da pesquisa serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+    if (!campoPesquisa) {
+      section.innerHTML = "<p>Nada foi encontrado</p>"
+      return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    // Imprime no console para fins de depuração (pode ser removido em produção)
+    console.log(section);
+  
+    // Inicializa uma string vazia para armazenar os resultados da pesquisa
+    let resultados = "";
+    let origem = ";"
+    let genero = "";
+    let caracteristica = "";
+    let evolucao = "";
+    let tags = "";
+  
+    // Itera sobre cada dado na lista de dados
+    for (let dado of dados) {
+      origem = dado.origem.toLowerCase()
+      genero = dado.genero.toLowerCase()
+      caracteristica = dado.caracteristica.toLowerCase()
+      evolucao = dado.evolucao.toLowerCase()
+      tags = dado.tags.toLowerCase()
+      if (origem.includes(campoPesquisa) || genero.includes(campoPesquisa) || caracteristica.includes(campoPesquisa) ||
+      evolucao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+      // Constrói o HTML para cada item do resultado
+        resultados += `
+        <div class="item-resultado">
+          <h2>
+            <a href="#" target="_blank">${dado.genero}</a>
+          </h2>
+          <p class="descricao-meta"><strong>Origem:</strong> ${dado.origem}</p>
+          <p class="descricao-meta"><strong>Caracterítica:</strong> ${dado.caracteristica}</p>
+          <p class="descricao-meta"><strong>Evolução:</strong> ${dado.evolucao}</p>
+          <a href="${dado.link}" target="_blank">Mais informações</a>
+        </div>`;
+        } 
+        if (!resultados) {
+          resultados = "<p>Busca não encontrada!</p>"
+        }
+    }
+  
+    // Atribui o HTML gerado para a seção de resultados
+    section.innerHTML = resultados;
+  }
+
---
+++ dados.js
@@ -0,0 +1,67 @@
+let dados = [
+    {
+        genero : "Sertanejo",
+        origem: "Brasil",
+        caracteristica: "Melodias românticas e letras que falam sobre amor, paixão e sofrimento. Ritmos dançantes e instrumentos como violão, viola e sanfona são comuns.",
+        evolucao: "Nos últimos anos, o sertanejo se popularizou ainda mais com a inclusão de elementos do pop e do eletrônico, criando um som mais moderno e com apelo para um público mais jovem. Artistas como Gusttavo Lima, Marília Mendonça e Henrique e Juliano são alguns dos principais nomes do gênero.",
+        link : "https://pt.wikipedia.org/wiki/Sertanejo",
+        tags: "sofrência, sofrencia, viola, arrocha"
+    },
+    {
+        genero : "Funk",
+        origem: "Favelas do Rio de Janeiro",
+        caracteristica: "Ritmos sincopados e batidas eletrônicas, letras que abordam temas do cotidiano, como amor, amizade, festas e vida nas comunidades.",
+        evolucao: " O funk se espalhou por todo o Brasil e se diversificou em diversos subgêneros, como o funk melody, o funk ostentação e o funk proibidão. Atualmente, é um dos estilos musicais mais populares entre os jovens brasileiros.",
+        link : "https://pt.wikipedia.org/wiki/Funk_(música)",
+        tags: "proibidão, batida, carioca, comunidade, MC"
+    },
+    {
+        genero : "Pop",
+        origem: "Estados Unidos",
+        caracteristica: "Melodias cativantes, letras que abordam temas universais como amor, felicidade e superação. Ritmos variados, que podem ir do eletrônico ao acústico.",
+        evolucao: " O pop é um gênero musical extremamente versátil e em constante evolução. Nos últimos anos, a influência de artistas como Ariana Grande, Billie Eilish e BTS ajudou a moldar o som do pop contemporâneo, com a inclusão de elementos do R&B, hip-hop e eletrônica.",
+        link : "https://pt.wikipedia.org/wiki/Música_pop",
+        tags: "hit, DJ, dance"
+    },
+    {
+        genero : "Rock",
+        origem: "Estados Unidos",
+        caracteristica: "Ritmos enérgicos, guitarras distorcidas, letras que abordam temas diversos, como amor, protesto e liberdade.",
+        evolucao: "O rock possui diversas vertentes, como o rock clássico, o rock alternativo e o heavy metal. Cada subgênero possui suas próprias características e influências.",
+        link : "https://pt.wikipedia.org/wiki/Rock",
+        tags: "punk, metal, bateria, baixo, guitarra, EUA"
+    },
+    {
+        genero : "Hip Hop",
+        origem: "Comunidades afro-americanas dos Estados Unidos",
+        caracteristica: "Combina rap, DJing, breakdance e grafite. Ritmos sincopados, letras que abordam temas sociais e culturais, como racismo, desigualdade e vida nas comunidades.",
+        evolucao: "Originário das comunidades afro-americanas dos Estados Unidos, o hip hop se espalhou pelo mundo e se diversificou em diversos subgêneros..",
+        link : "https://pt.wikipedia.org/wiki/Hip_hop",
+        tags:"rap, rima, social, realismo, luta, EUA"
+    },
+    {
+        genero: "Samba",
+        origem: "Brasil (Rio de Janeiro)",
+        caracteristica: "Ritmo sincopado, melodias alegres e letras que falam sobre amor, cotidiano e a vida na cidade. Instrumentos como cavaquinho, pandeiro, violão e surdo são essenciais.",
+        evolucao: "Originado nas comunidades negras do Rio de Janeiro, o samba se espalhou por todo o Brasil e se diversificou em diversos subgêneros, como samba-enredo, samba de roda, pagode e samba-rock. Atualmente, o samba continua sendo um dos ritmos mais populares do Brasil e influencia diversos outros gêneros musicais.",
+        link: "https://pt.wikipedia.org/wiki/Samba",
+        tags: "rio de janeiro, carnaval, cavaquinho, pagode, samba-enredo, samba de roda"
+      },
+      {
+        genero: "Pagode",
+        origem: "Brasil (Rio de Janeiro)",
+        caracteristica: "Ritmo mais lento e melodias românticas, com letras que falam sobre amor, amizade e cotidiano. Instrumentos como cavaquinho, pandeiro, violão e surdo são comuns, assim como a percussão mais marcada.",
+        evolucao: "Originado do samba, o pagode se popularizou nas décadas de 1980 e 1990, com grupos como Exaltasamba e Raça Negra. Atualmente, o pagode romântico é o mais popular, mas existem diversas vertentes como o pagode consciente e o pagode funk.",
+        link: "https://pt.wikipedia.org/wiki/Pagode",
+        tags: "samba, romantismo, amizade, cavaquinho, surdo, exaltasamba, raca negra"
+      },
+      {
+        genero: "Eletrônico",
+        origem: "Vários países (Europa, Estados Unidos, Japão)",
+        caracteristica: "Ritmos sintéticos, batidas repetitivas e melodias criadas por meio de instrumentos eletrônicos. A sonoridade é muito variada, abrangendo desde a música techno e house até o dubstep e o trance.",
+        evolucao: "Surgiu nas décadas de 1970 e 1980, com a criação dos primeiros sintetizadores e sequenciadores. A música eletrônica se popularizou mundialmente nos anos 1990 e continua em constante evolução, com a criação de novos subgêneros e a utilização de novas tecnologias.",
+        link: "https://pt.wikipedia.org/wiki/Música_eletrônica",
+        tags: "techno, house, dubstep, trance, eletrônico, sintetizador, dj, rave"
+      }
+
+];
